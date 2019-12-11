98aea63b1dbaefdc74891abb7fbfd804ebaedc18
maximaarminger <maxima.arminger@htl-salzburg.ac.at>
2019-12-11 11:20:59 +0100

    create Calculator

---
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeNumbers : MonoBehaviour
+{
+    public Text result;
+    public InputField ipfVarA;
+    public InputField ipfVarB;
+    public Button btnSubNumber;
+    public Button btnReset;
+
+    int index = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+      public void SubNumbersOnClick()
+    {
+
+        int variableA = int.Parse(ipfVarA.text);
+        int variableB = int.Parse(ipfVarB.text);
+        int SubResult = variableA - variableB;
+        result.text = SubResult.ToString();
+
+        ipfVarA.interactable = false;
+        ipfVarB.interactable = false;
+        btnSubNumber.interactable = false;
+        btnReset.interactable = true;
+
+
+    }
+
+    public void ResetOnClick()
+    {
+        ipfVarA.interactable = true;
+        ipfVarB.interactable = true;
+        btnSubNumber.interactable = true;
+        btnReset.interactable = false;
+
+        result.text = "result";
+        ipfVarA.text = "0";
+        ipfVarB.text = "0";
+
+    }
+
+
+}
