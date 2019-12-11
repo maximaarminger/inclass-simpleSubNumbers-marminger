1fc610d7dda233b61738a702a31f532e315bd166
maximaarminger <maxima.arminger@htl-salzburg.ac.at>
2019-12-11 11:28:04 +0100

    Beautify Script

--- Assets/MyGame/Scripts/ComputeNumbers.cs
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class ComputeNumbers : MonoBehaviour
@@ -11,19 +9,10 @@ public class ComputeNumbers : MonoBehaviour
     public Button btnSubNumber;
     public Button btnReset;
 
-    int index = 0;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        btnReset.interactable = false;
     }
 
       public void SubNumbersOnClick()
